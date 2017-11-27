3c2ae4a4ea58da6330e114d5812ee2ee3388a23b
Biellmelo <gabriell.demelo@outlook.com>
2017-11-27 03:16:16 -0200

    Loren

--- SISTEMA/Models/ApresentacaoContext.cs
+++ SISTEMA/Models/ApresentacaoContext.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using SABOR_E_SAUDE.DOMAIN.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace SISTEMA.Models
+namespace SISTEMA.Models 
 {
-    public class ApresentacaoContext 
+    public class ApresentacaoContext : DbContext
     {
+        public ApresentacaoContext() : base("DefaultConnection")
+        {
+
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Administrador> Administrador { get; set; }
+        // public DbSet<Carrinho> Carrinho { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        // public DbSet<ItemCarrinho> ItensCarrinhoss { get; set; }
+        public DbSet<Promocao> Promocoes { get; set; }
     }
 }
---
+++ SISTEMA/ViewModels/Cliente/ClienteViewModel.cs
@@ -0,0 +1,79 @@
+﻿using SABOR_E_SAUDE.DOMAIN.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SISTEMA.ViewModels.Cliente
+{
+    public class ClienteViewModel
+    {
+        public ClienteViewModel()
+        {
+            ClienteId = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid ClienteId { get; set; }
+
+
+        //Nome
+        [Required(ErrorMessage = "Favor, precisamos saber qual é o seu nome!.")]
+        [StringLength(50, MinimumLength = 5)]
+        [Display(Name = "* Nome")]
+        [Column(TypeName = "varchar")]
+        public string Nome { get; set; }
+
+        //E-mail
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Qual é o seu E-mail??")]
+        [StringLength(50, MinimumLength = 10)]
+        [Display(Name = "* E-mail:")]
+        [Column(TypeName = "varchar")]
+        public string Email { get; set; }
+
+        //DATA DE NASCIMENTO
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        //Permissões de Usuario
+        public string Permissao { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+
+        [Required]
+        public bool Excluido { get; set; }
+
+        //Senha
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Recomendamos hahaha (minimo 8, maximo 10)")]
+        [StringLength(20, MinimumLength = 8)]
+        [Display(Name = "* Senha:")]
+        [Column(TypeName = "varchar")]
+        public string Senha { get; set; }
+
+        //Senha
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "senhas não coincidem")]
+        [StringLength(20, MinimumLength = 8)]
+        [Display(Name = "* Confirmação de Senha:")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferes")]
+        public string ConfirmaSenha { get; set; }
+
+        //CPF
+        [Required(ErrorMessage = "CPF na nota é dinheiro de volta, preencha este campo.")]
+        [StringLength(11, MinimumLength = 11)]
+        [Display(Name = "* CPF:")]
+        public string CPF { get; set; }
+
+        //Data De Cadastro
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
